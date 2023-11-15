0e5538093836949f17876d84a7cca30c1446a5b1
Evaisa <evagiacosa1@gmail.com>
2023-11-15 16:45:32 +0100

    wah

---
+++ LethalThings/Plugin.cs
@@ -0,0 +1,135 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace LethalThings
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string ModGUID = "evaisa.lethalthings";
+        public const string ModName = "LethalThings";
+        public const string ModVersion = "0.1.0";
+
+        public static AssetBundle MainAssets;
+
+        [Flags]
+        public enum Levels
+        {
+            None = 1 << 0,
+            ExperimentationLevel = 1 << 1,
+            AssuranceLevel = 1 << 2,
+            VowLevel = 1 << 3,
+            OffenseLevel = 1 << 4,
+            MarchLevel = 1 << 5,
+            RendLevel = 1 << 6,
+            DineLevel = 1 << 7,
+            TitanLevel = 1 << 8,
+            All = ExperimentationLevel | AssuranceLevel | VowLevel | OffenseLevel | MarchLevel | RendLevel | DineLevel | TitanLevel
+        }
+
+        public class ScrapItem
+        {
+            public Item item;
+            public int rarity;
+            public Levels spawnLevels;
+
+            public ScrapItem(Item item, int rarity, Levels spawnLevels)
+            {
+                this.item = item;
+                this.rarity = rarity;
+                this.spawnLevels = spawnLevels;
+            }
+        }
+
+        public List<ScrapItem> scrapItems = new List<ScrapItem>();
+
+        private void Awake()
+        {
+
+            // find the file named arsonplush in the dll's folder
+            MainAssets = AssetBundle.LoadFromFile(Assembly.GetExecutingAssembly().Location.Replace("LethalThings.dll", "arsonplush"));
+
+            var plush = MainAssets.LoadAsset<Item>("Assets/Custom/LethalThings/Scrap/ArsonPlush.asset");
+
+            FixShaders(plush.spawnPrefab);
+
+            RegisterScrap(plush, 80, Levels.All);
+
+            On.StartOfRound.Awake += StartOfRound_Awake;
+
+            // Plugin startup logic
+            Logger.LogInfo($"Lethal things loaded!!");
+        }
+
+        public void FixShaders(GameObject gameObject)
+        {
+            foreach (var renderer in gameObject.GetComponentsInChildren<Renderer>())
+            {
+                foreach (var material in renderer.materials)
+                {
+                    if (material.shader.name.Contains("Standard"))
+                    {
+                        // ge
+                        material.shader = Shader.Find("HDRP/Lit");
+                    }
+                }
+            }
+        }
+
+        private void StartOfRound_Awake(On.StartOfRound.orig_Awake orig, StartOfRound self)
+        {
+            orig(self);
+
+            foreach(SelectableLevel level in self.levels)
+            {
+                var name = level.name;
+
+                if (Enum.IsDefined(typeof(Levels), name))
+                {
+                    var levelEnum = (Levels)Enum.Parse(typeof(Levels), name);
+                    foreach (ScrapItem scrapItem in scrapItems)
+                    {
+                        if (scrapItem.spawnLevels.HasFlag(levelEnum))
+                        {
+                            var spawnableÍtemWithRarity = new SpawnableItemWithRarity() { 
+                                spawnableItem = scrapItem.item,
+                                rarity = scrapItem.rarity
+                            };
+
+                            // make sure spawnableScrap does not already contain item
+                            Logger.LogInfo($"Checking if {scrapItem.item.name} is already in {name}");
+
+                            if (!level.spawnableScrap.Any(x => x.spawnableItem == scrapItem.item))
+                            {
+                                level.spawnableScrap.Add(spawnableÍtemWithRarity);
+                                Logger.LogInfo($"Added {scrapItem.item.name} to {name}");
+                            }
+                        }
+                    }
+                }
+            }
+
+
+
+            foreach (ScrapItem scrapItem in scrapItems)
+            {
+                if(!self.allItemsList.itemsList.Contains(scrapItem.item))
+                {
+                    Logger.LogInfo($"Item registered: {scrapItem.item.name}");
+                    self.allItemsList.itemsList.Add(scrapItem.item);
+                }
+            }
+
+         }
+
+        public void RegisterScrap(Item spawnableItem, int rarity, Levels levelFlags)
+        {
+            var scrapItem = new ScrapItem(spawnableItem, rarity, levelFlags);
+            scrapItems.Add(scrapItem);
+        }
+    }
+}
---
+++ Unity/LethalCompanyProject/Library/PackageCache/com.unity.visualscripting@1.9.0/Runtime/VisualScripting.State/FlowState.cs
@@ -0,0 +1,99 @@
+using System.ComponentModel;
+using UnityEngine;
+
+namespace Unity.VisualScripting
+{
+    [SerializationVersion("A")]
+    [TypeIcon(typeof(FlowGraph))]
+    [DisplayName("Script State")]
+    public sealed class FlowState : NesterState<FlowGraph, ScriptGraphAsset>, IGraphEventListener
+    {
+        public FlowState() { }
+
+        public FlowState(ScriptGraphAsset macro) : base(macro) { }
+
+
+        #region Lifecycle
+
+        protected override void OnEnterImplementation(Flow flow)
+        {
+            if (flow.stack.TryEnterParentElement(this))
+            {
+                nest.graph.StartListening(flow.stack);
+                flow.stack.TriggerEventHandler(hook => hook == StateEventHooks.OnEnterState, new EmptyEventArgs(), parent => parent is SubgraphUnit, false);
+                flow.stack.ExitParentElement();
+            }
+        }
+
+        protected override void OnExitImplementation(Flow flow)
+        {
+            if (flow.stack.TryEnterParentElement(this))
+            {
+                flow.stack.TriggerEventHandler(hook => hook == StateEventHooks.OnExitState, new EmptyEventArgs(), parent => parent is SubgraphUnit, false);
+                (nest.graph as IGraphEventListener).StopListening(flow.stack, true);
+                flow.stack.ExitParentElement();
+            }
+        }
+
+        public void StartListening(GraphStack stack)
+        {
+            if (stack.TryEnterParentElement(this))
+            {
+                nest.graph.StartListening(stack);
+                stack.ExitParentElement();
+            }
+        }
+
+        void IGraphEventListener.StopListening(GraphStack stack, bool destroyed)
+            => StopListening(stack, destroyed);
+
+        public void StopListening(GraphStack stack) => StopListening(stack, true);
+
+        public void StopListening(GraphStack stack, bool destroyed)
+        {
+            if (stack.TryEnterParentElement(this))
+            {
+                (nest.graph as IGraphEventListener).StopListening(stack, destroyed);
+                stack.ExitParentElement();
+            }
+        }
+
+        public bool IsListening(GraphPointer pointer)
+        {
+            return pointer.GetElementData<Data>(this).isActive;
+        }
+
+        #endregion
+
+
+        #region Factory
+
+        public override FlowGraph DefaultGraph()
+        {
+            return GraphWithEnterUpdateExit();
+        }
+
+        public static FlowState WithEnterUpdateExit()
+        {
+            var flowState = new FlowState();
+            flowState.nest.source = GraphSource.Embed;
+            flowState.nest.embed = GraphWithEnterUpdateExit();
+            return flowState;
+        }
+
+        public static FlowGraph GraphWithEnterUpdateExit()
+        {
+            return new FlowGraph
+            {
+                units =
+                {
+                    new OnEnterState { position = new Vector2(-205, -215) },
+                    new Update { position = new Vector2(-161, -38) },
+                    new OnExitState { position = new Vector2(-205, 145) }
+                }
+            };
+        }
+
+        #endregion
+    }
+}
