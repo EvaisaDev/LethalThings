6931a7daf2d58c5d50bb215bd5de89b341e927ef
Evaisa <evagiacosa1@gmail.com>
2023-11-16 11:41:31 +0100

    wah

--- LethalThings/Plugin.cs
+++ LethalThings/Plugin.cs
@@ -1,9 +1,16 @@
 ﻿using BepInEx;
+using HarmonyLib;
+using LethalLib.Modules;
+using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using Unity.Netcode;
 using UnityEngine;
+using static LethalLib.Modules.Enemies;
 
 namespace LethalThings
 {
@@ -16,120 +23,25 @@ namespace LethalThings
 
         public static AssetBundle MainAssets;
 
-        [Flags]
-        public enum Levels
-        {
-            None = 1 << 0,
-            ExperimentationLevel = 1 << 1,
-            AssuranceLevel = 1 << 2,
-            VowLevel = 1 << 3,
-            OffenseLevel = 1 << 4,
-            MarchLevel = 1 << 5,
-            RendLevel = 1 << 6,
-            DineLevel = 1 << 7,
-            TitanLevel = 1 << 8,
-            All = ExperimentationLevel | AssuranceLevel | VowLevel | OffenseLevel | MarchLevel | RendLevel | DineLevel | TitanLevel
-        }
-
-        public class ScrapItem
-        {
-            public Item item;
-            public int rarity;
-            public Levels spawnLevels;
-
-            public ScrapItem(Item item, int rarity, Levels spawnLevels)
-            {
-                this.item = item;
-                this.rarity = rarity;
-                this.spawnLevels = spawnLevels;
-            }
-        }
-
-        public List<ScrapItem> scrapItems = new List<ScrapItem>();
-
         private void Awake()
         {
-
+           
             // find the file named arsonplush in the dll's folder
-            MainAssets = AssetBundle.LoadFromFile(Assembly.GetExecutingAssembly().Location.Replace("LethalThings.dll", "arsonplush"));
+            MainAssets = AssetBundle.LoadFromFile(Assembly.GetExecutingAssembly().Location.Replace("LethalThings.dll", "lethalthings"));
 
             var plush = MainAssets.LoadAsset<Item>("Assets/Custom/LethalThings/Scrap/ArsonPlush.asset");
+            var enemy = MainAssets.LoadAsset<EnemyType>("Assets/Custom/LethalThings/Enemies/Boomba.asset");
+            var terminalKeyword = MainAssets.LoadAsset<TerminalKeyword>("Assets/Custom/LethalThings/Enemies/BoombaTerminal.asset");
+            var terminalNode = MainAssets.LoadAsset<TerminalNode>("Assets/Custom/LethalThings/Enemies/BoombaFile.asset");
 
-            FixShaders(plush.spawnPrefab);
-
-            RegisterScrap(plush, 80, Levels.All);
-
-            On.StartOfRound.Awake += StartOfRound_Awake;
-
-            // Plugin startup logic
-            Logger.LogInfo($"Lethal things loaded!!");
+            LethalLib.Modules.NetworkPrefabs.RegisterNetworkPrefab(plush.spawnPrefab);
+            Items.RegisterScrap(plush, 80, Levels.LevelTypes.All);
+            
+            /*Enemies.RegisterEnemy(enemy, 80, Levels.LevelTypes.All, SpawnType.Default, terminalKeyword, terminalNode);
+            Enemies.RegisterEnemy(enemy, 100, Levels.LevelTypes.All, SpawnType.Outside);
+            Enemies.RegisterEnemy(enemy, 100, Levels.LevelTypes.All, SpawnType.Daytime);*/
         }
 
-        public void FixShaders(GameObject gameObject)
-        {
-            foreach (var renderer in gameObject.GetComponentsInChildren<Renderer>())
-            {
-                foreach (var material in renderer.materials)
-                {
-                    if (material.shader.name.Contains("Standard"))
-                    {
-                        // ge
-                        material.shader = Shader.Find("HDRP/Lit");
-                    }
-                }
-            }
-        }
 
-        private void StartOfRound_Awake(On.StartOfRound.orig_Awake orig, StartOfRound self)
-        {
-            orig(self);
-
-            foreach(SelectableLevel level in self.levels)
-            {
-                var name = level.name;
-
-                if (Enum.IsDefined(typeof(Levels), name))
-                {
-                    var levelEnum = (Levels)Enum.Parse(typeof(Levels), name);
-                    foreach (ScrapItem scrapItem in scrapItems)
-                    {
-                        if (scrapItem.spawnLevels.HasFlag(levelEnum))
-                        {
-                            var spawnableÍtemWithRarity = new SpawnableItemWithRarity() { 
-                                spawnableItem = scrapItem.item,
-                                rarity = scrapItem.rarity
-                            };
-
-                            // make sure spawnableScrap does not already contain item
-                            Logger.LogInfo($"Checking if {scrapItem.item.name} is already in {name}");
-
-                            if (!level.spawnableScrap.Any(x => x.spawnableItem == scrapItem.item))
-                            {
-                                level.spawnableScrap.Add(spawnableÍtemWithRarity);
-                                Logger.LogInfo($"Added {scrapItem.item.name} to {name}");
-                            }
-                        }
-                    }
-                }
-            }
-
-
-
-            foreach (ScrapItem scrapItem in scrapItems)
-            {
-                if(!self.allItemsList.itemsList.Contains(scrapItem.item))
-                {
-                    Logger.LogInfo($"Item registered: {scrapItem.item.name}");
-                    self.allItemsList.itemsList.Add(scrapItem.item);
-                }
-            }
-
-         }
-
-        public void RegisterScrap(Item spawnableItem, int rarity, Levels levelFlags)
-        {
-            var scrapItem = new ScrapItem(spawnableItem, rarity, levelFlags);
-            scrapItems.Add(scrapItem);
-        }
     }
 }
---
+++ LethalThings/RoombaAI.cs
@@ -0,0 +1,457 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Unity.Netcode;
+using UnityEngine;
+using GameNetcodeStuff;
+using Random = UnityEngine.Random;
+using static UnityEngine.GraphicsBuffer;
+using MonoMod.Cil;
+using System.Diagnostics;
+using System.Reflection;
+using Debug = UnityEngine.Debug;
+
+namespace LethalThings
+{
+    public class RoombaAI : EnemyAI
+    {
+        public override void Start()
+        {
+            base.Start();
+            this.nearPlayerColliders = new Collider[4];
+        }
+
+        public override void DoAIInterval()
+        {
+            base.DoAIInterval();
+            if (StartOfRound.Instance.livingPlayers == 0)
+            {
+                return;
+            }
+            if (this.isEnemyDead)
+            {
+                return;
+            }
+            int currentBehaviourStateIndex = this.currentBehaviourStateIndex;
+            if (currentBehaviourStateIndex != 0)
+            {
+                if (currentBehaviourStateIndex != 1)
+                {
+                    return;
+                }
+                this.CheckForVeryClosePlayer();
+                if (this.lostPlayerInChase)
+                {
+                    this.movingTowardsTargetPlayer = false;
+                    if (!this.searchForPlayers.inProgress)
+                    {
+                        this.searchForPlayers.searchWidth = 30f;
+                        base.StartSearch(this.lastPositionOfSeenPlayer, this.searchForPlayers);
+                        Debug.Log("Crawler: Lost player in chase; beginning search where the player was last seen");
+                        return;
+                    }
+                }
+                else if (this.searchForPlayers.inProgress)
+                {
+                    base.StopSearch(this.searchForPlayers, true);
+                    this.movingTowardsTargetPlayer = true;
+                    Debug.Log("Crawler: Found player during chase; stopping search coroutine and moving after target player");
+                }
+            }
+            else if (!this.searchForPlayers.inProgress)
+            {
+                base.StartSearch(base.transform.position, this.searchForPlayers);
+                Debug.Log(string.Format("Crawler: Started new search; is searching?: {0}", this.searchForPlayers.inProgress));
+                return;
+            }
+        }
+
+        public override void FinishedCurrentSearchRoutine()
+        {
+            base.FinishedCurrentSearchRoutine();
+            this.searchForPlayers.searchWidth = Mathf.Clamp(this.searchForPlayers.searchWidth + 10f, 1f, this.maxSearchAndRoamRadius);
+        }
+
+
+
+
+        public override void Update()
+        {
+            base.Update();
+            if (this.isEnemyDead)
+            {
+                return;
+            }
+            if (!base.IsOwner)
+            {
+                this.inSpecialAnimation = false;
+            }
+            this.CalculateAgentSpeed();
+            if (GameNetworkManager.Instance.localPlayerController.HasLineOfSightToPosition(base.transform.position + Vector3.up * 0.25f, 80f, 25, 5f))
+            {
+                if (this.currentBehaviourStateIndex == 1)
+                {
+                    GameNetworkManager.Instance.localPlayerController.IncreaseFearLevelOverTime(0.8f, 1f);
+                }
+                else
+                {
+                    GameNetworkManager.Instance.localPlayerController.IncreaseFearLevelOverTime(0.8f, 0.5f);
+                }
+            }
+            int currentBehaviourStateIndex = this.currentBehaviourStateIndex;
+            if (currentBehaviourStateIndex != 0)
+            {
+                if (currentBehaviourStateIndex != 1)
+                {
+                    return;
+                }
+                if (!this.hasEnteredChaseMode)
+                {
+                    this.hasEnteredChaseMode = true;
+                    this.lostPlayerInChase = false;
+                    this.checkLineOfSightInterval = 0f;
+                    this.noticePlayerTimer = 0f;
+                    this.beginningChasingThisClient = false;
+                    this.useSecondaryAudiosOnAnimatedObjects = true;
+                    this.openDoorSpeedMultiplier = 1.5f;
+                    this.agent.stoppingDistance = 0.5f;
+                    this.agent.speed = 0f;
+                }
+                if (!base.IsOwner)
+                {
+                    return;
+                }
+                if (this.stunNormalizedTimer > 0f)
+                {
+                    return;
+                }
+                if (this.checkLineOfSightInterval <= 0.075f)
+                {
+                    this.checkLineOfSightInterval += Time.deltaTime;
+                    return;
+                }
+                this.checkLineOfSightInterval = 0f;
+                if (this.inSpecialAnimation)
+                {
+                    return;
+                }
+                if (this.lostPlayerInChase)
+                {
+                    PlayerControllerB playerControllerB = base.CheckLineOfSightForPlayer(55f, 60, -1);
+                    if (playerControllerB)
+                    {
+                        this.noticePlayerTimer = 0f;
+                        this.lostPlayerInChase = false;
+                        this.MakeScreechNoiseServerRpc();
+                        if (playerControllerB != this.targetPlayer)
+                        {
+                            base.SetMovingTowardsTargetPlayer(playerControllerB);
+                            base.ChangeOwnershipOfEnemy(playerControllerB.actualClientId);
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        this.noticePlayerTimer -= 0.075f;
+                        if (this.noticePlayerTimer < -15f)
+                        {
+                            base.SwitchToBehaviourState(0);
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    PlayerControllerB playerControllerB2 = base.CheckLineOfSightForPlayer(65f, 80, -1);
+                    if (playerControllerB2 != null)
+                    {
+                        this.noticePlayerTimer = 0f;
+                        this.lastPositionOfSeenPlayer = playerControllerB2.transform.position;
+                        if (playerControllerB2 != this.targetPlayer)
+                        {
+                            this.targetPlayer = playerControllerB2;
+                            base.ChangeOwnershipOfEnemy(this.targetPlayer.actualClientId);
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        this.noticePlayerTimer += 0.075f;
+                        if (this.noticePlayerTimer > 1.5f)
+                        {
+                            this.lostPlayerInChase = true;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (this.hasEnteredChaseMode)
+                {
+                    this.hasEnteredChaseMode = false;
+                    this.searchForPlayers.searchWidth = 25f;
+                    this.beginningChasingThisClient = false;
+                    this.noticePlayerTimer = 0f;
+                    this.useSecondaryAudiosOnAnimatedObjects = false;
+                    this.openDoorSpeedMultiplier = 0.6f;
+                    this.agent.stoppingDistance = 0f;
+                    this.agent.speed = 7f;
+                }
+                if (this.checkLineOfSightInterval <= 0.05f)
+                {
+                    this.checkLineOfSightInterval += Time.deltaTime;
+                    return;
+                }
+                this.checkLineOfSightInterval = 0f;
+                PlayerControllerB playerControllerB3;
+                if (this.stunnedByPlayer != null)
+                {
+                    playerControllerB3 = this.stunnedByPlayer;
+                    this.noticePlayerTimer = 1f;
+                }
+                else
+                {
+                    playerControllerB3 = base.CheckLineOfSightForPlayer(55f, 60, -1);
+                }
+                if (!(playerControllerB3 == GameNetworkManager.Instance.localPlayerController))
+                {
+                    this.noticePlayerTimer -= Time.deltaTime;
+                    return;
+                }
+                this.noticePlayerTimer = Mathf.Clamp(this.noticePlayerTimer + 0.05f, 0f, 10f);
+                if (this.noticePlayerTimer > 0.2f && !this.beginningChasingThisClient)
+                {
+                    this.beginningChasingThisClient = true;
+                    this.BeginChasingPlayerServerRpc((int)GameNetworkManager.Instance.localPlayerController.playerClientId);
+                    base.ChangeOwnershipOfEnemy(playerControllerB3.actualClientId);
+                    Debug.Log("Begin chasing on local client");
+                    return;
+                }
+            }
+        }
+
+        private void CalculateAgentSpeed()
+        {
+            if (this.stunNormalizedTimer >= 0f)
+            {
+                this.agent.speed = 0.1f;
+                this.agent.acceleration = 200f;
+                //this.creatureAnimator.SetBool("stunned", true);
+                return;
+            }
+            //this.creatureAnimator.SetBool("stunned", false);
+            //this.creatureAnimator.SetFloat("speedMultiplier", Vector3.ClampMagnitude(base.transform.position - this.previousPosition, 1f).sqrMagnitude / (Time.deltaTime / 2.25f));
+            float num = (this.previousPosition - base.transform.position).sqrMagnitude / (Time.deltaTime / 1.4f);
+            if (base.IsOwner && this.previousVelocity - num > Mathf.Clamp(num * 0.25f, 2f, 100f))
+            {
+                this.agent.speed = 0f;
+                if (this.currentBehaviourStateIndex == 1)
+                {
+                    if (this.wallCollisionSFXDebounce > 0.5f)
+                    {
+                        if (base.IsServer)
+                        {
+                            this.CollideWithWallServerRpc();
+                        }
+                        else
+                        {
+                            this.CollideWithWallClientRpc();
+                        }
+                    }
+                    this.wallCollisionSFXDebounce = 0f;
+                }
+            }
+            this.wallCollisionSFXDebounce += Time.deltaTime;
+            this.previousVelocity = num;
+            this.previousPosition = base.transform.position;
+            if (this.currentBehaviourStateIndex == 0)
+            {
+                this.agent.speed = 8f;
+                this.agent.acceleration = 26f;
+                return;
+            }
+            if (this.currentBehaviourStateIndex == 1)
+            {
+                this.agentSpeedWithNegative += Time.deltaTime * 1.5f;
+                this.agent.speed = Mathf.Clamp(this.agentSpeedWithNegative, -3f, 16f);
+                this.agent.acceleration = Mathf.Clamp(45f - num * 12f, 4f, 80f);
+            }
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        public void CollideWithWallServerRpc()
+        {
+            NetworkManager networkManager = base.NetworkManager;
+            if (networkManager == null || !networkManager.IsListening)
+            {
+                return;
+            }
+            if (IsServer || (!networkManager.IsServer && !networkManager.IsHost))
+            {
+                return;
+            }
+            this.CollideWithWallClientRpc();
+        }
+
+        [ClientRpc]
+        public void CollideWithWallClientRpc()
+        {
+            NetworkManager networkManager = base.NetworkManager;
+            if (networkManager == null || !networkManager.IsListening)
+            {
+                return;
+            }
+            if (IsClient || (!networkManager.IsClient && !networkManager.IsHost))
+            {
+                return;
+            }
+            RoundManager.PlayRandomClip(this.creatureSFX, this.hitWallSFX, true, 1f, 0);
+            float num = Vector3.Distance(GameNetworkManager.Instance.localPlayerController.transform.position, base.transform.position);
+            if (num < 15f)
+            {
+                HUDManager.Instance.ShakeCamera(ScreenShakeType.Big);
+                return;
+            }
+            if (num < 24f)
+            {
+                HUDManager.Instance.ShakeCamera(ScreenShakeType.Small);
+            }
+        }
+
+        private void CheckForVeryClosePlayer()
+        {
+            if (Physics.OverlapSphereNonAlloc(base.transform.position, 1.5f, this.nearPlayerColliders, 8, QueryTriggerInteraction.Ignore) <= 0)
+            {
+                return;
+            }
+            PlayerControllerB component = this.nearPlayerColliders[0].transform.GetComponent<PlayerControllerB>();
+            if (component != null && component != this.targetPlayer && !Physics.Linecast(base.transform.position + Vector3.up * 0.3f, component.transform.position, StartOfRound.Instance.collidersAndRoomMask))
+            {
+                this.targetPlayer = component;
+            }
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        public void BeginChasingPlayerServerRpc(int playerObjectId)
+        {
+            NetworkManager networkManager = base.NetworkManager;
+            if (networkManager == null || !networkManager.IsListening)
+            {
+                return;
+            }
+            if (this.IsServer || (!networkManager.IsServer && !networkManager.IsHost))
+            {
+                return;
+            }
+            this.BeginChasingPlayerClientRpc(playerObjectId);
+        }
+
+        [ClientRpc]
+        public void BeginChasingPlayerClientRpc(int playerObjectId)
+        {
+            NetworkManager networkManager = base.NetworkManager;
+            if (networkManager == null || !networkManager.IsListening)
+            {
+                return;
+            }
+            if (this.IsClient || (!networkManager.IsClient && !networkManager.IsHost))
+            {
+                return;
+            }
+            this.MakeScreech();
+            base.SwitchToBehaviourStateOnLocalClient(1);
+            base.SetMovingTowardsTargetPlayer(StartOfRound.Instance.allPlayerScripts[playerObjectId]);
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        public void MakeScreechNoiseServerRpc()
+        {
+            NetworkManager networkManager = base.NetworkManager;
+            if (networkManager == null || !networkManager.IsListening)
+            {
+                return;
+            }
+            if (IsServer || (!networkManager.IsServer && !networkManager.IsHost))
+            {
+                return;
+            }
+            this.MakeScreechNoiseClientRpc();
+        }
+
+        [ClientRpc]
+        public void MakeScreechNoiseClientRpc()
+        {
+            NetworkManager networkManager = base.NetworkManager;
+            if (networkManager == null || !networkManager.IsListening)
+            {
+                return;
+            }
+            if (IsClient || (!networkManager.IsClient && !networkManager.IsHost))
+            {
+                return;
+            }
+            this.MakeScreech();
+        }
+
+        private void MakeScreech()
+        {
+            int num = Random.Range(0, this.longRoarSFX.Length);
+            this.creatureVoice.PlayOneShot(this.longRoarSFX[num]);
+            WalkieTalkie.TransmitOneShotAudio(this.creatureVoice, this.longRoarSFX[num], 1f);
+            if (Vector3.Distance(GameNetworkManager.Instance.localPlayerController.transform.position, base.transform.position) < 15f)
+            {
+                GameNetworkManager.Instance.localPlayerController.JumpToFearLevel(0.75f, true);
+            }
+        }
+
+        public override void OnCollideWithPlayer(Collider other)
+        {
+            base.OnCollideWithPlayer(other);
+
+        }
+
+
+
+        public AISearchRoutine searchForPlayers;
+
+        private float checkLineOfSightInterval;
+
+        public float maxSearchAndRoamRadius = 100f;
+
+        [Space(5f)]
+        public float noticePlayerTimer;
+
+        private bool hasEnteredChaseMode;
+
+        private bool lostPlayerInChase;
+
+        private bool beginningChasingThisClient;
+
+        private Collider[] nearPlayerColliders;
+
+        public AudioClip shortRoar;
+
+        public AudioClip[] hitWallSFX;
+
+        public AudioClip bitePlayerSFX;
+
+        private Vector3 previousPosition;
+
+        private float previousVelocity;
+
+        private float wallCollisionSFXDebounce;
+
+        public AudioClip[] hitCrawlerSFX;
+
+        public AudioClip[] longRoarSFX;
+
+        public DeadBodyInfo currentlyHeldBody;
+
+        private bool pullingSecondLimb;
+
+        private float agentSpeedWithNegative;
+
+        private Vector3 lastPositionOfSeenPlayer;
+    }
+}
